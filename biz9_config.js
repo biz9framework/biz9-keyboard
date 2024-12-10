4fde6656a0aaf82daa42cba6dd036f415591f596
think1 <think1@localhost.localdomain>
2024-12-09 23:42:10 -0500

    good

--- biz9_config.js
+++ biz9_config.js
@@ -1,4 +1,4 @@
-exports.VERSION="1.0.15"
+exports.VERSION="1.0.17"
 exports.TITLE='BiZ9-Keyboard'
 exports.REPO='git@github.com:biz9framework/biz9-keyboard.git'
-exports.BRANCH='1.0.1'
+exports.BRANCH='1.2.0'
